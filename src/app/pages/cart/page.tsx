cddea8732384d0046a2e48fd070c6ede0411d090
Talnetd Prince <princensude6@gmail.com>
2025-11-03 23:14:02 +0100

    cart spinner logic

--- src/app/pages/cart/page.tsx
+++ src/app/pages/cart/page.tsx
@@ -25,10 +25,11 @@ export default function CartPage() {
     console.log("Checkout button clicked");
     const token = localStorage.getItem("token");
     const seletedItem = items.filter((item) => item.selected);
-    setIsLoading(true);
+
     if (!token) {
       toast.error("Please log in to checkout", { position: 'top-right' });
       console.log("token", token)
+      setIsLoading(false);
       setTimeout(() => {
         setSingin(true);
       }, 500);
@@ -44,6 +45,7 @@ export default function CartPage() {
     if (token && seletedItem.length > 0 && items.length > 0) {
       setIsAdress(true);
       setSingin(false);
+      setIsLoading(true);
     }
     setTimeout(() => {
       setIsLoading(false);
