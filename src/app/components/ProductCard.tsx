07e986c11b083e46b261cbf646f5bb952c249bd7
J-ARGS-INC <nsudekenechi2@gmail.com>
2026-01-12 14:02:59 +0100

    made new changes

--- src/app/components/ProductCard.tsx
+++ src/app/components/ProductCard.tsx
@@ -62,7 +62,7 @@ export default function ProductCard({
             src={product.cover_image}
             alt="product image"
             fill
-            className="transition-transform duration-300 ease-in-out group-hover:scale-110"
+            className="transition-transform duration-300 ease-in-out group-hover:scale-110 object-cover"
           />
         </div>
       </Link>
