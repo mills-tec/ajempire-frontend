dc427480afed859219932818d53b9bad057bac56
Talnetd Prince <princensude6@gmail.com>
2026-01-26 08:44:55 +0100

    returns component

---
+++ src/app/components/SelectedItemSkeleton.tsx
@@ -0,0 +1,8 @@
+export default function SelectedItemSkeleton() {
+    return (
+        <div className="animate-pulse">
+            <div className="size-[4rem] rounded-md bg-gray-300"></div>
+            <div className="h-3 w-12 bg-gray-200 rounded mt-2"></div>
+        </div>
+    );
+}
